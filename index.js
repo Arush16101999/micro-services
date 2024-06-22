f68639abb47a8affc0c5daca8ad6971787779f63
arushmano <61136045+Arush16101999@users.noreply.github.com>
2024-06-22 19:50:41 +0530

    micro service basic setup

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 import { ServiceBroker } from "moleculer";
 
 const broker = new ServiceBroker();
+
+//greeter Service
+broker.createService({
+  name: "greeter",
+  actions: {
+    sayHello(ctx) {
+      return `Hi Buddy! ${ctx.params.name}!`;
+    },
+  },
+});
+
+async function startApp() {
+  await broker.start();
+  const res = await broker.call("greeter.sayHello", { name: "Arush" });
+  console.log(res);
+  broker.stop();
+}
+
+startApp();
